e09c3db79a6240ad550357a573817a6cac16640f
livia-oliveira <livia0liveiraslv93@gmail.com>
2025-05-01 16:45:47 -0300

    refactor: usa traid ApiResponse nas respostas do controller de Subtarefas

--- app/Http/Controllers/SubtarefasController.php
+++ app/Http/Controllers/SubtarefasController.php
@@ -4,34 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\Subtarefa;
 use Illuminate\Http\Request;
+use App\Traits\ApiResponse;
 
 class SubtarefasController extends Controller
 {
+    use ApiResponse;
+
     public function index()
     {
-        return Subtarefa::all();
+        $subtarefas = Subtarefa::all();
+        return $this->success($subtarefas);
     }
 
     public function store(Request $request)
     {
         $subtarefa = Subtarefa::create($request->only(['titulo', 'status', 'tarefa_id']));
-        return response()->json($subtarefa, 201);
+        return $this->success($subtarefa, 'Subtarefa criada com sucesso', 201);
     }
 
     public function show(Subtarefa $subtarefa)
     {
-        return $subtarefa;
+        return $this->success($subtarefa, 'Subtarefa encontrada');
     }
 
     public function update(Request $request, Subtarefa $subtarefa)
     {
         $subtarefa->update($request->only(['titulo', 'status']));
-        return response()->json($subtarefa);
+        return $this->success($subtarefa, 'Subtarefa atualizada com sucesso');
     }
 
     public function destroy(Subtarefa $subtarefa)
     {
         $subtarefa->delete();
-        return response()->json(null, 204);
+        return $this->success(null, 'Tarefa deletada com sucesso', 204);
     }
 }
